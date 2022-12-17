7dfb54d81bc960bcdcd7b44938d921926bba74db
andrey <andrey@DESKTOP-DQN4G8E>
2022-12-17 17:42:12 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace SurFractal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Crimson;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Tai Le", 11.8209F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(908, 652);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Наложить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 1);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1067, 647);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.SlateBlue;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Tai Le", 11.8209F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(808, 652);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1075, 692);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fractal";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace SurFractal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+        public void drawF(int x, int y,double len,double angle,Color col,float w,int white,Color sec)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            double x1, y1;
+            x1 = x + len * Math.Sin(angle * Math.PI * 2 / 360.0);
+            y1 = y + len * Math.Cos(angle * Math.PI * 2 / 360.0);
+            g.DrawLine(new Pen(col, w), x, pictureBox1.Height - y, (int)x1, pictureBox1.Height - (int)y1);
+            
+            if (len > 2 & len < 10)
+            {
+                drawF((int)x1, (int)y1, len / (rnd.NextDouble() + 1), angle + rnd.Next(0, 40),sec,w/3,white,sec);
+                drawF((int)x1, (int)y1, len / (rnd.NextDouble() + 1), angle - rnd.Next(0, 40),sec,w/3,white, sec);
+            }
+            if (len > 10 & len < 50)
+            {
+                drawF((int)x1, (int)y1, len / (rnd.NextDouble() + 1), angle + rnd.Next(0, 40),Color.FromArgb(white + 30, white + 30, white + 30), w/2,white, sec);
+                drawF((int)x1, (int)y1, len / (rnd.NextDouble() + 1), angle - rnd.Next(0, 40),Color.FromArgb(white + 30, white + 30, white + 30), w/2,white, sec);
+            }
+            if (len > 50)
+            {
+                drawF((int)x1, (int)y1, (len / (rnd.NextDouble() + 1)), angle + rnd.Next(0, 40),Color.FromArgb(white + 30, white + 30, white + 30),w,white, sec);
+                drawF((int)x1, (int)y1, (len / (rnd.NextDouble() + 1)), angle - rnd.Next(0, 40),Color.FromArgb(white + 30, white + 30, white + 30), w,white, sec);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DrawAsync();
+        }
+         async void DrawAsync()
+        {
+            int ang = 0;
+            while (ang < 360)
+            {
+                await Task.Run(() => drawF(pictureBox1.Width / 2, pictureBox1.Height / 2, 15.0 + rnd.Next(30, 60), ang, Color.FromArgb(255, 255, 255), 2 + rnd.Next(1, 4), 225, Color.BlueViolet));
+                ang += rnd.Next(20, 35);
+            }
+            ang = 0;
+            while (ang < 360){
+                await Task.Run(() => drawF(pictureBox1.Width / 2, pictureBox1.Height / 2, 15.0 + rnd.Next(30, 60), ang, Color.FromArgb(255, 255, 255), 2 + rnd.Next(1, 4), 225, Color.HotPink));
+                ang += rnd.Next(20, 35);
+            }
+            ang = 0;
+            while (ang < 360)
+            {
+                await Task.Run(() => drawF(pictureBox1.Width / 2, pictureBox1.Height / 2, 15.0 + rnd.Next(30, 60), ang, Color.FromArgb(255, 255, 225), 2 + rnd.Next(1, 4), 225, Color.OrangeRed));
+                ang += rnd.Next(20, 35);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
